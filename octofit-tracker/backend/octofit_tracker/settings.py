6fd49ab390c358b6c8f8ce97d5a2bc692fa88c62
Alexander Chepkiyeng <50344667+Xanderyeng@users.noreply.github.com>
2025-04-08 03:40:32 +0000

    Feat: Step 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-4+&!@#%$^&*()_+=-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['refactored-space-guacamole-5x77xvj7jxqhpgvr-8000.app.github.dev', 'localhost']
 
 # Application definition
 
